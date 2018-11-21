64cc59aac9d9a8e2581efc87e42d78320b9580b3
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-21 20:38:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,15 @@
 def oxford_comma(array)
-   if array.length == 1
-    array.join
-  elsif array.length == 2
-    array.join(" and ")
-  else
-    last_element = array.pop
-    array.join(", ") << ", and " + last_element
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.size == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.size > 3
+    position=1
+    string1=""
+    array.each {|name| (position < array.size) ? string1 << "#{array[position-1]}, " : string1 << "and #{array[position-1]}" ; position += 1}
+    return string1
   end
+ end 
+end
