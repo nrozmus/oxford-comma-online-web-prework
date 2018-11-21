8fa65f5d0a9953d981774610f6fdbf759a7d8682
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-21 20:35:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 def oxford_comma(array)
- end 
   if array.length == 1
-    array.join
+    return array.join
   elsif array.length == 2
-    array.join (" and ")
-  else array.length > 2
-    last = array.slice!(-1)
-    sentence = array.join(", ")
-    sentence.to_s + ", and " + last.to_s
-  end
+    return array.join" and "
+  else
+    last_element = array.pop
+    array_string = array.join(", ")
+    return array_string << ", and " + "#{last_element}"
+ end 
+end
 end
