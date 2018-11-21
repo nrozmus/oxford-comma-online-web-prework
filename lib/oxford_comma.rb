f9d9c0794a7b0b44f89e1c906c76b41b6473f101
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-21 20:37:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,10 @@
 def oxford_comma(array)
-  array.join(",")
-  if array.size == 0
-    "Nothing to join."
-  elsif array.size == 1
-    array[0].to_s
-  elsif array.size == 2
-    array.join(" and ")
-  elsif array.size == 3
-    last = array.pop
-    array.join(", ").to_s << (", and #{last}")
-  else
-    last = array.pop
-    array.join(", ").to_s << (", and #{last}")
-  
-  end
  end 
- end
+  if array.length == 1
+    return ["kiwi"].join
+  elsif array.length == 2
+    return ["kiwi", "durian"].join(" and ")
+  else
+    array[0...-1].join(", ") <<", and #{array[-1]}"
+  end
+end
