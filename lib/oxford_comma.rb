3ef8dac319e66c5947603ede3a93ad97de125539
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-21 20:39:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_item = array.pop
+    array_as_string = array.join(", ")
+    array_as_string += ", and #{last_item}"
+  end
 end
